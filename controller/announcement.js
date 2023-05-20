f1ced5046da0e504124e4e9703f9aaa85e7f9999
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-05-20 14:04:37 +0500

    announcement error fix at catch

--- controller/announcement.js
+++ controller/announcement.js
@@ -1,22 +1,21 @@
 import announcement from '../database/announcement.js'
 import { InternalServerError, ValidationError } from '../utils/errors.js'
 announcement.sync({ force: false })
+
 const createAnnouncement = async (req, res, next) => {
     try {
-        const newAnnouncement = await announcement.create(req.body)
-        res.status(201).send(newAnnouncement, {
+        await announcement.create(req.body)
+        res.status(201).send({
             message: "Successfully created"
         })
     } catch (error) {
-        return next(new ValidationError(400, error.message))
+        return next(new ValidationError(400, error))
     }
 }
-const getAllAnnouncement = async (req, res) => {
+const getAllAnnouncement = async (req, res, next) => {
     try {
         const announcements = await announcement.findAll()
-        res.status(200).send(announcements, {
-            message: "Successfully fetched"
-        })
+        res.status(200).send(announcements)
     } catch (error) {
         return next(new InternalServerError(500, error.message))
     }
